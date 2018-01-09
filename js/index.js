f807f70e03599c82a6c6bc7cfcd7a191ff66f69f
Tracy <me@tracyodell.com>
2018-01-09 11:42:17 -0800

    removed google and added parallax

--- js/index.js
+++ js/index.js
@@ -4,59 +4,64 @@ $(function() {
     $("#searchTerm").keypress(function(e){
   $("#output").empty();
 
-    	if(e.keyCode===13){
-    		var searchTerm = $("#searchTerm").val();
-		    var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchTerm +"&format=json&callback=?"; 
-		    $.ajax({
-			url: url,
-			type: 'GET',
-			contentType: "application/json",
-			async: false,
-        	dataType: "json",
-        	success: function(data, status, jqXHR) {
-        		//console.log(data);
-        		$("#output").html();
-        		for(var i=0;i<data[1].length;i++){
-        			$("#output").prepend("<div><div class='well'><a href="+data[3][i]+"><h2>" + data[1][i]+ "</h2>" + "<p>" + data[2][i] + "</p></a></div></div>");
-        		}
+        if(e.keyCode===13){
+            var searchTerm = $("#searchTerm").val();
+            var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchTerm +"&format=json&callback=?"; 
+            $.ajax({
+            url: url,
+            type: 'GET',
+            contentType: "application/json",
+            async: false,
+            dataType: "json",
+            success: function(data, status, jqXHR) {
+                //console.log(data);
+                $("#output").html();
+                for(var i=0;i<data[1].length;i++){
+                    $("#output").prepend("<div><div class='well'><a href="+data[3][i]+"><h3>" + data[1][i]+ "</h3>" + "<p>" + data[2][i] + "</p></a></div></div>");
+                }
 
-        	}
-		})
-    	}
+            }
+        })
+        }
     });
   
 // click ajax call
     $("#search").on("click", function() {
       $("#output").empty();
-    	var searchTerm = $("#searchTerm").val();
-		var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchTerm +"&format=json&callback=?"; 
-		$.ajax({
-			url: url,
-			type: 'GET',
-			contentType: "application/json",
-			async: false,
-        	dataType: "json",
+        var searchTerm = $("#searchTerm").val();
+        var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchTerm +"&format=json&callback=?"; 
+        $.ajax({
+            url: url,
+            type: 'GET',
+            contentType: "application/json",
+            async: false,
+            dataType: "json",
           // plop data
-        	success: function(data, status, jqXHR) {
-        		//console.log(data);
-        		$("#output").html();
-        		for(var i=0;i<data[1].length;i++){
-//          		for(var i=0;i<3;i++){
+            success: function(data, status, jqXHR) {
+                $("#output").html();
+              if (data[1].length == 0) {
+                $("#output").prepend("<div><div class='well'><h4>sorry, no results found</h4></div></div>");
+              }
+            
+            for(var i=0;i<data[1].length;i++){
+            //for(var i=0;i<3;i++){
 
-            $("#output").prepend("<div><div class='well'><a href="+data[3][i]+"><h2>" + data[1][i]+ "</h2>" + "<p>" + data[2][i] + "</p></a></div></div>");
-        		}
+            $("#output").prepend("<div><div class='well'><a href="+data[3][i]+"><h3>" + data[1][i]+ "</h3>" + "<p>" + data[2][i] + "</p></a></div></div>");
+                }
 
-        	}
-		})
-		.done(function() {
-			console.log("success");
-		})
-		.fail(function() {
-			console.log("error");
-		})
-		.always(function() {
-			console.log("complete");
-		});
-		     
+            }
+        })
+        .done(function() {
+            console.log("success");
+        })
+        .fail(function() {
+            console.log("error");
+        })
+        .always(function() {
+            console.log("complete");
+        });
+             
     });
 });
+
+
